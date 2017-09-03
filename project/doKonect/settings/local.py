43aa5b3a6d67ca3af466bb33375966a32c6caea0
Ahmedsebit <Ahmed.yusuf@andela.com>
2017-09-03 16:51:45 +0300

    ADD views

--- project/doKonect/settings/local.py
+++ project/doKonect/settings/local.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static-some/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static-storage")
