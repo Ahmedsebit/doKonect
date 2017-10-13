0b52865b4d6d03dbc69a6e4027135e7b8a39fb52
Ahmedsebit <Ahmed.yusuf@andela.com>
2017-10-13 12:44:22 +0300

    ADD get post api for clinic

---
+++ project/clinic/api/serializers.py
@@ -0,0 +1,50 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
+from clinic.models import Clinic_Date, Clinic_Date_Patients
+from rest_framework import serializers
+from accounts.api.serializers import UserDisplaySerializer 
+from patients.api.serializers import PatientVisitSerializer
+
+
+class Clinic_DateDisplaySerializer(serializers.ModelSerializer):
+    # url = serializers.SerializerMethodField()
+    class Meta:
+        model = Clinic_Date
+        user_id = UserDisplaySerializer(read_only=True)
+        fields = [
+            'clinic_name',
+            'clinic_details',
+            'user_id',
+            'booking_date',
+            'clinic_date',
+            'doctor_accept',
+            'clinic_accept',
+            'clinic_type'
+        ]
+
+    def get_referal_count(self, obj):
+        return 0
+
+    def get_url(self, obj):
+        return reverse_lazy("clinic:list", kwargs={"username":obj.username})
+
+
+class Clinic_Date_PatientDisplaySerializer(serializers.ModelSerializer):
+    # url = serializers.SerializerMethodField()
+    class Meta:
+        model = Clinic_Date_Patients
+        user = UserDisplaySerializer(read_only=True)
+        clinic = Clinic_DateDisplaySerializer(read_only=True)
+        patient = PatientVisitSerializer(read_only=True)
+        fields = [
+            'clinic',
+            'user',
+            'patient',
+            'date'
+        ]
+
+    def get_referal_count(self, obj):
+        return 0
+
+    def get_url(self, obj):
+        return reverse_lazy("clinic:list", kwargs={"username":obj.username})
---
+++ project/clinic/api/urls.py
@@ -0,0 +1,33 @@
+from django.conf.urls import url
+from .views import (
+    Clinic_Date_DoctorApiCreateView,
+    Clinic_Date_ApiCreateView,
+    # Clinic_Date_ApiUpdateView,
+    # Clinic_Date_ApiDetailView,
+    # Clinic_Date_ClinicApiDetailView,
+    # Clinic_Date_DoctorApiDetailView,
+    # Clinic_Date_MainClinicApiDetailView, 
+    Clinic_Date_ApiListView,
+    Clinic_Date_DoctorApiListView,
+    Clinic_Date_ClinicApiListView,
+    # accept_view,
+    # reject_view,
+    # addpatient_view
+)
+
+urlpatterns = [
+    url(r'^myclinic/create/$', Clinic_Date_ApiCreateView.as_view(), name='doctorcreate'),
+    url(r'^create/$', Clinic_Date_ApiCreateView.as_view(), name='create'),
+    # url(r'^(?P<pk>\d+)/update/$', Clinic_Date_ApiUpdateView.as_view(), name='update'),
+    url(r'^$', Clinic_Date_ApiListView.as_view(), name='list'),
+    url(r'^myclinic/$', Clinic_Date_DoctorApiListView.as_view(), name='doctorlist'),
+    url(r'^clinics/$', Clinic_Date_ClinicApiListView.as_view(), name='cliniclist'),
+    # url(r'^(?P<pk>\d+)/$', Clinic_Date_ApiDetailView.as_view(), name='detail'),
+    # url(r'viewclinic/(?P<pk>\d+)/$', Clinic_Date_ClinicApiDetailView.as_view(), name='clinicdetail'),
+    # url(r'myscheduledclinic/(?P<pk>\d+)/$', Clinic_Date_DoctorApiDetailView.as_view(), name='clinicddetail'),
+    # url(r'^scheduledclinic/(?P<pk>\d+)/$', Clinic_Date_MainClinicApiDetailView.as_view(), name='cdetail'),
+    # url(r'^my_objects/(?P<id>\d+)/$', accept_view, name='accepted'),
+    # url(r'^my_objects/(?P<id>\d+)/$', reject_view, name='rejected'),
+    # url(r'^viewclinic/add_patient/(?P<clinic_id>\d+)/(?P<patient_id>\d+)/$', addpatient_view, name='add_patient')
+]
+
